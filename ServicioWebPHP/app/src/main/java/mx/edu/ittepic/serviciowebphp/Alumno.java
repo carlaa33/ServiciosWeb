fb8720e8a6fb36914f2cac0f10865ba0e58961c9
carlita <carla.cutte@hotmail.com>
2018-04-12 09:30:08 -0600

    Final

--- ServicioWebPHP/app/src/main/java/mx/edu/ittepic/serviciowebphp/Alumno.java
+++ ServicioWebPHP/app/src/main/java/mx/edu/ittepic/serviciowebphp/Alumno.java
@@ -1,5 +1,7 @@
 package mx.edu.ittepic.serviciowebphp;
 
+import android.os.AsyncTask;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,37 +9,39 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.Switch;
 import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
 
 public class Alumno extends AppCompatActivity {
     String accion;
     TextView titulo;
-    Button buscar, aceptar, volver;
+    Button buscar, aceptar, volver,MT;
     EditText id,nombre,direccion;
     URL url;
     String socket;
 
     LinearLayout layin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alumno);
 
-        socket="w.x.y.z:8888";
+        socket="192.168.10.2";
 
         layin=findViewById(R.id.layin);
 
@@ -54,213 +58,35 @@ public class Alumno extends AppCompatActivity {
 
         inicializar(accion);
 
-        volver.setOnClickListener(new View.OnClickListener() {
+        volver.setOnClickListener(new Button.OnClickListener(){
             @Override
             public void onClick(View view) {
                 finish();
             }
         });
 
-        aceptar.setOnClickListener(new View.OnClickListener() {
+        aceptar.setOnClickListener(new Button.OnClickListener(){
             @Override
             public void onClick(View view) {
-                accion();
+                new Conexion().execute();
+                finish();
             }
         });
 
-        buscar.setOnClickListener(new View.OnClickListener() {
+        buscar.setOnClickListener(new Button.OnClickListener(){
             @Override
             public void onClick(View view) {
-                buscar();
+                accion="Buscar";
+                new Conexion().execute();
             }
         });
-    }
-
-    private void buscar() {
-        String URL="http://"+socket+"/datos1/obtener_alumno_por_id.php?idalumno="+id.getText();
-        String cadena="";
-        try {
-            URL url = new URL(URL);
-            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-            int respuesta = httpURLConnection.getResponseCode();
-
-            InputStream inputStream = httpURLConnection.getInputStream();
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-            StringBuilder stringBuilder = new StringBuilder();
-
-            if (respuesta == HttpURLConnection.HTTP_OK) {
-                while ((URL = bufferedReader.readLine()) != null) {
-                    stringBuilder.append(URL + "\n");
-                }
-                bufferedReader.close();
-                inputStream.close();
-                httpURLConnection.disconnect();
-                cadena = stringBuilder.toString().trim();
-            }
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        Log.e("JSON",cadena);
-    }
-
-    private void accion() {
-        switch(accion) {
-            case "Nuevo":
-                nuevo();
-                break;
-            case "Eliminar":
-                eliminar();
-                break;
-            case "Editar":
-                editar();
-                break;
-        }
-
-    }
-
-    private void editar() {
-        HttpURLConnection conn = null;
-        try {
-            String URL="http://"+socket+"/datos1/actualizar_alumno.php";
-
-            // Objeto url que recibe como parametro la cadena al webservice de Insertar
-            url = new URL(URL);
-
-            // Cast para manejar la conexión como un objeto HttpURLConnection
-            conn = (HttpURLConnection) url.openConnection();
-
-            // Establecimiento de un tipo de conexión POST
-            conn.setRequestMethod("POST");
-
-            // Propiedades de la conexión
-            conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
-            conn.setRequestProperty("Accept","application/json");
-            conn.setDoOutput(true);
-            conn.setDoInput(true);
-
-            // Creación de Objetos JSON y carga de datos
-            JSONObject jsonn = new JSONObject();
-            jsonn.put("idalumno", id.getText().toString());
-            jsonn.put("nombre", nombre.getText().toString());
-            jsonn.put("direccion", direccion.getText().toString());
-
-            // Tambien puede usarse utilizando OutputStream
-            DataOutputStream os = new DataOutputStream(conn.getOutputStream());
-            os.writeBytes(jsonn.toString());
-
-            os.flush();
-            os.close();
-
-            conn.disconnect();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        } finally {
-            if (conn != null) {
-                conn.disconnect();
-            }
-        }
-    }
-
-    private void eliminar() {
-        HttpURLConnection conn = null;
-        try {
-            String URL="http://"+socket+"/datos1/borrar_alumno.php";
-
-            // Objeto url que recibe como parametro la cadena al webservice de Insertar
-            url = new URL(URL);
-
-            // Cast para manejar la conexión como un objeto HttpURLConnection
-            conn = (HttpURLConnection) url.openConnection();
-
-            // Establecimiento de un tipo de conexión POST
-            conn.setRequestMethod("POST");
-
-            // Propiedades de la conexión
-            conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
-            conn.setRequestProperty("Accept","application/json");
-            conn.setDoOutput(true);
-            conn.setDoInput(true);
-
-            // Creación de Objetos JSON y carga de datos
-            JSONObject jsonn = new JSONObject();
-            jsonn.put("idalumno", id.getText().toString());
-
-            // Tambien puede usarse utilizando OutputStream
-            DataOutputStream os = new DataOutputStream(conn.getOutputStream());
-            os.writeBytes(jsonn.toString());
-
-            os.flush();
-            os.close();
-
-            conn.disconnect();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        } finally {
-            if (conn != null) {
-                conn.disconnect();
-            }
-        }
-    }
-
-    private void nuevo() {
-        HttpURLConnection conn = null;
-        try {
-            String URL="http://localhost/api/alumnos";
-
-            // Objeto url que recibe como parametro la cadena al webservice de Insertar
-            url = new URL(URL);
-
-            // Cast para manejar la conexión como un objeto HttpURLConnection
-            conn = (HttpURLConnection) url.openConnection();
-
-            // Establecimiento de un tipo de conexión POST
-            conn.setRequestMethod("POST");
-
-            // Propiedades de la conexión
-            conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
-            conn.setRequestProperty("Accept","application/json");
-            conn.setDoOutput(true);
-            conn.setDoInput(true);
-
-            // Creación de Objetos JSON y carga de datos
-            JSONObject jsonn = new JSONObject();
-            jsonn.put("_id", id.getText().toString());
-            jsonn.put("nombre", nombre.getText().toString());
-            jsonn.put("direccion", direccion.getText().toString());
-
-            // Tambien puede usarse utilizando OutputStream
-            DataOutputStream os = new DataOutputStream(conn.getOutputStream());
-            os.writeBytes(jsonn.toString());
-
-            os.flush();
-            os.close();
-
-            conn.disconnect();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        } finally {
-            if (conn != null) {
-                conn.disconnect();
-            }
-        }
     }
 
     private void inicializar(String accion) {
         titulo.setText(accion);
         switch(accion) {
             case "Nuevo":
+                layin.setVisibility(View.INVISIBLE);
                 aceptar.setText("Guardar");
                 break;
             case "Eliminar":
@@ -280,4 +106,213 @@ public class Alumno extends AppCompatActivity {
         }
 
     }
+
+
+    public class Conexion extends AsyncTask<Void, Void, String> {
+        private String buscar() {
+            String URL="http://"+socket+"/datos1/obtener_alumno_por_id.php?idalumno="+id.getText();
+            String cadena="";
+            try {
+                URL url = new URL(URL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                int respuesta = httpURLConnection.getResponseCode();
+
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                StringBuilder stringBuilder = new StringBuilder();
+
+                if (respuesta == HttpURLConnection.HTTP_OK) {
+                    while ((URL = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(URL + "\n");
+                    }
+                    bufferedReader.close();
+                    inputStream.close();
+                    httpURLConnection.disconnect();
+                    cadena = stringBuilder.toString().trim();
+                    cadena=cadena.replace("{\"estado\":1,\"alumno\":","");
+                    cadena=cadena.replace("\"}}","\"}");
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (Exception e){
+                Log.e("Error",e.getMessage());
+            }
+            return cadena;
+        }
+
+        private void editar() {
+            HttpURLConnection conn = null;
+            try {
+                String URL="http://"+socket+"/datos1/actualizar_alumno.php";
+
+                String json_string;
+                StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                StrictMode.setThreadPolicy(policy);
+                URL url = new URL(URL);
+                HttpURLConnection connection =  (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("POST");
+                JSONObject json = new JSONObject();
+                json.put("idalumno", id.getText().toString());
+                json.put("nombre", nombre.getText().toString());
+                json.put("direccion", direccion.getText().toString());
+                OutputStream os = connection.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os,"UTF-8"));
+                writer.write(json.toString());
+                writer.flush();
+                writer.close();
+                os.close();
+                int respuesta = 0;
+                respuesta = connection.getResponseCode();
+                InputStream inputStream = connection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                StringBuilder stringBuilder = new StringBuilder();
+                if (respuesta == HttpURLConnection.HTTP_OK) {
+                    while ((json_string = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(json_string + "\n");
+                    }
+                    bufferedReader.close();
+                    inputStream.close();
+                    connection.disconnect();
+                }
+            }catch (java.net.MalformedURLException e){
+
+            }catch (IOException e) {
+
+            }catch (org.json.JSONException e){
+
+            }
+        }
+
+        private void eliminar() {
+            HttpURLConnection conn = null;
+            try {
+                String URL="http://"+socket+"/datos1/borrar_alumno.php";
+
+                String json_string;
+                StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                StrictMode.setThreadPolicy(policy);
+                URL url = new URL(URL);
+                HttpURLConnection connection =  (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("POST");
+                JSONObject json = new JSONObject();
+                json.put("idalumno",id.getText().toString());
+                OutputStream os = connection.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os,"UTF-8"));
+                writer.write(json.toString());
+                writer.flush();
+                writer.close();
+                os.close();
+                int respuesta = 0;
+                respuesta = connection.getResponseCode();
+                InputStream inputStream = connection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                StringBuilder stringBuilder = new StringBuilder();
+                if (respuesta == HttpURLConnection.HTTP_OK) {
+                    while ((json_string = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(json_string + "\n");
+                    }
+                    bufferedReader.close();
+                    inputStream.close();
+                    connection.disconnect();
+                }
+            }catch (java.net.MalformedURLException e){
+
+            }catch (IOException e) {
+
+            }catch (org.json.JSONException e){
+
+            }
+        }
+
+        private void nuevo() {
+            try {
+
+                String URL="http://"+socket+"/datos1/insertar_alumno.php";
+                String json_string;
+                StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                StrictMode.setThreadPolicy(policy);
+                URL url = new URL(URL);
+                HttpURLConnection connection =  (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("POST");
+                JSONObject json = new JSONObject();
+                json.put("nombre",nombre.getText().toString());
+                json.put("direccion",direccion.getText().toString());
+                OutputStream os = connection.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os,"UTF-8"));
+                writer.write(json.toString());
+                writer.flush();
+                writer.close();
+                os.close();
+                int respuesta = 0;
+                respuesta = connection.getResponseCode();
+                InputStream inputStream = connection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                StringBuilder stringBuilder = new StringBuilder();
+                if (respuesta == HttpURLConnection.HTTP_OK) {
+                    while ((json_string = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(json_string + "\n");
+                    }
+                    bufferedReader.close();
+                    inputStream.close();
+                    connection.disconnect();
+                }
+            }catch (java.net.MalformedURLException e){
+
+            }catch (IOException e) {
+
+            }catch (org.json.JSONException e){
+
+            }
+        }
+
+        @Override
+        protected String doInBackground(Void... Voids) {
+            String tmp="";
+
+            switch(accion) {
+                case "Nuevo":
+                    nuevo();
+                    break;
+                case "Eliminar":
+                    eliminar();
+                    break;
+                case "Editar":
+                    editar();
+                    break;
+                case "Buscar":
+                    tmp=buscar();
+            }
+            return  tmp;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if(!result.isEmpty()){
+                try {
+                    JSONObject alumno = new JSONObject(result);
+                    nombre.setText(alumno.get("nombre").toString());
+                    direccion.setText(alumno.get("direccion").toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            accion=titulo.getText().toString();
+
+        }
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+    }
 }
+
--- ServicioWebPHP/app/src/main/java/mx/edu/ittepic/serviciowebphp/MainActivity.java
+++ ServicioWebPHP/app/src/main/java/mx/edu/ittepic/serviciowebphp/MainActivity.java
@@ -1,17 +1,42 @@
 package mx.edu.ittepic.serviciowebphp;
 
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     ListView lista;
 
     Button mostrar,nuevo,eliminar,editar,buscar;
+    String socket;
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        new MostrarTodos().execute();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +48,8 @@ public class MainActivity extends AppCompatActivity {
         eliminar=findViewById(R.id.btnEliminar);
         editar=findViewById(R.id.btnEditar);
         buscar=findViewById(R.id.btnBuscar);
+        lista=findViewById(R.id.LV);
+        socket="192.168.10.2";
 
         mostrar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -89,7 +116,75 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void mostrar() {
-        //MostrarTodo
+        new MostrarTodos().execute();
+
+    }
+
+    public class MostrarTodos extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... Voids) {
+            String tmp="";
+            String URL="http://"+socket+"/datos1/obtener_alumnos.php";
+            try {
+                java.net.URL url = new URL(URL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                int respuesta = httpURLConnection.getResponseCode();
+
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                StringBuilder stringBuilder = new StringBuilder();
+
+                if (respuesta == HttpURLConnection.HTTP_OK) {
+                    while ((URL = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(URL + "\n");
+                    }
+                    bufferedReader.close();
+                    inputStream.close();
+                    httpURLConnection.disconnect();
+                    tmp = stringBuilder.toString().trim();
+                    tmp=tmp.replace("{\"estado\":1,\"alumnos\":","{alumnos:");
+
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (Exception e){
+                Log.e("Error",e.getMessage());
+            }
+            return tmp;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            String a="";
+            try {
+                JSONObject json = new JSONObject(result);
+                JSONArray jArray = json.getJSONArray("alumnos");for(int i=0; i<jArray.length(); i++){
+                    JSONObject datos = jArray.getJSONObject(i);
+
+                    a+= datos.getString("idalumno")+" "+datos.getString("nombre")+" "+datos.getString("direccion")+"\n";
+                }
+            } catch (JSONException e) {
+                Log.e("Error",e.getMessage());
+            }
+
+            String nLista[]=a.split("\n");
+            ArrayAdapter<String> adaptador = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, nLista);
+            lista.setAdapter(adaptador);
+
+        }
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
     }
 
 
